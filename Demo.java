b2611176efeec522e45eda6d813a95de8db80aad
unknown <savitrijambagi07@gmail.com>
2025-03-28 12:03:57 +0530

    modified

--- Demo.java
+++ Demo.java
@@ -1,8 +1,9 @@
 public class Demo
 {
-	public static void main(String args[])
+	public static void main(String args[]);
 	{
-		System.out.println("vidya");
+		System.out.println("c b kore polytechnic chikodi");
 	}
+	System.out.println("");
 	
 }
